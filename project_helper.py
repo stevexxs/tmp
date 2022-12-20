d75c47413b90787a29bbafd73951bd47f7f7d7f7
stevexxs <josling87@163.com>
2022-12-20 22:33:44 +0900

    Add files via upload

--- project_helper.py
+++ project_helper.py
@@ -1,5 +1,11 @@
+import alphalens as al
+import graphviz
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
+from IPython.display import Image
+from sklearn.tree import export_graphviz
 from zipline.assets._assets import Equity  # Required for USEquityPricing
 from zipline.pipeline.data import USEquityPricing
 from zipline.pipeline.classifiers import Classifier
@@ -30,7 +36,7 @@ class Sector(Classifier):
     missing_value = -1
 
     def __init__(self):
-        self.data = np.load('../../data/project_4_sector/data.npy')
+        self.data = np.load('../../data/project_7_sector/data.npy')
 
     def _compute(self, arrays, dates, assets, mask):
         return np.where(
@@ -51,5 +57,78 @@ def build_pipeline_engine(bundle_data, trading_calendar):
     return engine
 
 
-def get_factor_exposures(factor_betas, weights):
-    return factor_betas.loc[weights.index].T.dot(weights)
+def plot_tree_classifier(clf, feature_names=None):
+    dot_data = export_graphviz(
+        clf,
+        out_file=None,
+        feature_names=feature_names,
+        filled=True,
+        rounded=True,
+        special_characters=True,
+        rotate=True)
+
+    return Image(graphviz.Source(dot_data).pipe(format='png'))
+
+
+def plot(xs, ys, labels, title='', x_label='', y_label=''):
+    for x, y, label in zip(xs, ys, labels):
+        plt.ylim((0.5, 0.55))
+        plt.plot(x, y, label=label)
+
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    plt.legend(bbox_to_anchor=(1.04, 1), borderaxespad=0)
+    plt.show()
+
+
+def rank_features_by_importance(importances, feature_names):
+    indices = np.argsort(importances)[::-1]
+    max_feature_name_length = max([len(feature) for feature in feature_names])
+
+    print('      Feature{space: <{padding}}      Importance'.format(padding=max_feature_name_length - 8, space=' '))
+
+    for x_train_i in range(len(importances)):
+        print('{number:>2}. {feature: <{padding}} ({importance})'.format(
+            number=x_train_i + 1,
+            padding=max_feature_name_length,
+            feature=feature_names[indices[x_train_i]],
+            importance=importances[indices[x_train_i]]))
+
+
+def sharpe_ratio(factor_returns, annualization_factor=np.sqrt(252)):
+    return annualization_factor * factor_returns.mean() / factor_returns.std()
+
+
+def get_factor_returns(factor_data):
+    ls_factor_returns = pd.DataFrame()
+
+    for factor, factor_data in factor_data.items():
+        ls_factor_returns[factor] = al.performance.factor_returns(factor_data).iloc[:, 0]
+
+    return ls_factor_returns
+
+
+def plot_factor_returns(factor_returns):
+    (1 + factor_returns).cumprod().plot(ylim=(0.8, 1.2))
+
+
+def plot_factor_rank_autocorrelation(factor_data):
+    ls_FRA = pd.DataFrame()
+
+    unixt_factor_data = {
+        factor: factor_data.set_index(pd.MultiIndex.from_tuples(
+            [(x.timestamp(), y) for x, y in factor_data.index.values],
+            names=['date', 'asset']))
+        for factor, factor_data in factor_data.items()}
+
+    for factor, factor_data in unixt_factor_data.items():
+        ls_FRA[factor] = al.performance.factor_rank_autocorrelation(factor_data)
+
+    ls_FRA.plot(title="Factor Rank Autocorrelation", ylim=(0.8, 1.0))
+
+
+def build_factor_data(factor_data, pricing):
+    return {factor_name: al.utils.get_clean_factor_and_forward_returns(factor=data, prices=pricing, periods=[1])
+        for factor_name, data in factor_data.iteritems()}
